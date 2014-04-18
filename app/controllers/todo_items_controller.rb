120d5c8bc24ec74047b6c14492993ef01f4d934f
ryangoins <ryan.c.goins@gmail.com>
2014-04-18 08:51:53 -0400

    Refactored templates with partial

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,16 +1,16 @@
 class TodoItemsController < ApplicationController
+
+  before_action :find_todo_list
+
   def index
-  	@todo_list = TodoList.find(params[:todo_list_id])
+  	
   end
 
   def new
-
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new(todo_item_params)
   	if @todo_item.save
   		flash[:success] = "Added todo list item."
@@ -22,26 +22,27 @@ class TodoItemsController < ApplicationController
   end
 
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
-  def update
-    @todo_list = TodoList.find(params[:todo_list_id])
+  def update 
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
       flash[:success] = "Saved todo list item."
       redirect_to todo_list_todo_items_path
     else
-      flash[:error] = "That todo item could not be saved"
+    
       render action: :edit
     end
   end
-  
+
   def url_options
     { todo_list_id: params[:todo_list_id] }.merge(super)
   end
 
+  def find_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
   private
   def todo_item_params
   	params[:todo_item].permit(:content)
