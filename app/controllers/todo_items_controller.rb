7e201286f38d8ba057e6241a12b4568cf4e702c6
ryangoins <ryan.c.goins@gmail.com>
2014-04-21 19:37:48 -0400

    Delete todo list items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -31,9 +31,21 @@ class TodoItemsController < ApplicationController
       flash[:success] = "Saved todo list item."
       redirect_to todo_list_todo_items_path
     else
-    
+      flash[:error] = "That todo item could not be saved"
       render action: :edit
     end
+  end
+
+
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "Todo list item was deleted."
+    else 
+      flash[:error] = "Todo list item could not be deleted."
+    end
+    redirect_to todo_list_todo_items_path
+
   end
 
   def url_options
