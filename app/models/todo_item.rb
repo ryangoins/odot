dd6114ad04c337fce1faaa807fd00db4b7553c1d
ryangoins <ryan.c.goins@gmail.com>
2014-04-21 20:44:01 -0400

    Clean up todo mark complete todo

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,5 +2,10 @@ class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
   validates :content, presence: true,
   					  length: { minimum: 2 }
-  					 
+  
+
+  def completed? 
+  	!completed_at.blank?
+  end
+
 end
