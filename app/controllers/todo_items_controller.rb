b9c2684f63fac058eaac947d24c16e41386d53c8
ryangoins <ryan.c.goins@gmail.com>
2014-04-21 20:14:58 -0400

    Mark todo list item complete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -3,7 +3,6 @@ class TodoItemsController < ApplicationController
   before_action :find_todo_list
 
   def index
-  	
   end
 
   def new
@@ -46,6 +45,12 @@ class TodoItemsController < ApplicationController
     end
     redirect_to todo_list_todo_items_path
 
+  end
+
+  def complete
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to todo_list_todo_items_path, notice: "Todo item marekd as complete."
   end
 
   def url_options
